e4ae288f3f70b32a20f84adce082a984b6873e20
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-09-16 20:11:29 +0530

    implement update user, delete user and reset password and get user id inide the controller class

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -22,7 +22,6 @@ module.exports.login = async (req, res) => {
       });
     }
 
-    // bcrypting the password and comparing with the one in db
     if (await bcrypt.compare(password, user.password)) {
 
       const token = generateAuthToken({_id : user?._id, email : email})
@@ -51,7 +50,6 @@ module.exports.register = async (req, res) => {
   try {
     const { email, password, name, userType } = req.body;
     console.log(req.body)
-    // if any one of the field from email and password is not filled
     if (!email || !password) {
       return res.json({
         success: false,
@@ -73,3 +71,95 @@ module.exports.register = async (req, res) => {
   }
 };
 
+module.exports.updateUser = async (req, res) => {
+  try {
+
+    const userDataToBeUpdated = req.body;
+    const { id } = req.query;
+    const user = await userModel.findOne({ _id: id });
+
+    if (!user) return res.send("user does not exist");
+
+    let updatedUser = await userModel.findOneAndUpdate(
+      { _id: id },
+      userDataToBeUpdated,
+      { new: true }
+    );
+
+    return res.json({
+      success: true,
+      message: "user updated successfully",
+      data: updatedUser,
+    });
+  } catch (error) {
+    return res.send("error : ", error.message);
+  }
+};
+
+module.exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const user = await userModel.findOne({ _id: id });
+    if (!user) return res.status(200).send("user does not exist");
+
+    await userModel.findOneAndDelete({ _id: id });
+    
+    return res.json({
+      success: true,
+      message: "user deleted successfully",
+    });
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
+
+module.exports.userById = async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const user = await userModel.findOne({_id : id})
+    if(!user) return res.send("user does not exist")
+
+    return res.json({
+        success : true,
+        message : "user deleted successfully",
+        data : user
+    })
+
+    }catch(error){
+        return res.send("error : ", error.message)
+    }
+}
+
+module.exports.resetPassword = async (req, res) => {
+
+    try{
+        const {password, newPassword} = req.body;
+        const {id} = req.query
+    
+        if(!password || !newPassword || !id) return res.send("Fields are empty")
+    
+        let user = await userModel.findOne({_id : id})
+    
+        if(!user) return res.send("user does not exist")
+    
+        if(bcrypt.compare(password, user?.password)){
+            user.password = await bcrypt.hash(newPassword,10)
+            user.save()
+            return res.json({
+                success : true,
+                message : "password updated successfully"
+            })
+        }
+
+        return res.json({
+            success : false,
+            message : "wrong password"
+        })
+
+    }catch(error){
+        return res.send(error.message)
+    }
+    
+}
