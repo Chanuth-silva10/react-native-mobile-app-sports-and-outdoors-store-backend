555adf1a9b5cb49d0f70dd45bae413f61f16234d
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-09-16 20:15:00 +0530

    implement product model class

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const productSchema = mongoose.Schema({
+
+    title : String,
+    sku : {type : String},
+    price : Number,
+    image : String,
+    description : String,
+    quantity : Number
+
+}, { timestamps : true })
+
+module.exports = mongoose.model("product", productSchema)
