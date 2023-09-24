9d4355d1516f395ab20edfeb8894e7bf8414f208
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-09-24 11:49:42 +0530

    fix db error

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 const mogoose = require('mongoose')
 
+const { DB_CON_STRING } = process.env
+
 module.exports = () => {
+    // mogoose.connect("mongodb://localhost/ecommerce")
     mogoose.connect("mongodb+srv://chanuth1234:1yjFeKrcfqvMt1mW@cluster0.o5kob.mongodb.net/ecommerce")
         .then(() => console.log('DB Connection Successfull'))
         .catch(err => console.log(err.message))
